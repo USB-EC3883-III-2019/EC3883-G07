179aa56e6090a5dfd65b8143ef90ac004e042346
Eisler Aguilar <39389514+Eisler18@users.noreply.github.com>
2019-10-28 14:44:23 -0400

    Add files via upload

--- software/Pruebas/grafica.py
+++ software/Pruebas/grafica.py
@@ -7,7 +7,6 @@ import matplotlib.figure as Figure
 import numpy as np
 import time
 import serial
-from drawnow import drawnow
 from matplotlib.backends.qt_compat import QtWidgets
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas 
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
@@ -17,16 +16,7 @@ qtCreatorFile = "Solindar.ui" #Importo el archivo de designer
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 s = serial.Serial('COM8')
-s.baudrate = 115200
-
-
-i = 1
-n = 0
-flag = True
-fourx = np.linspace(-40, 40, 40)
-foury = np.sqrt(1600-(fourx*fourx))
-fourt = np.arctan2(foury,fourx)
-fourr = (fourx**2 + foury**2)**(0.5)
+s.baudrate = 116736
 
 class MyApp(QtGui.QMainWindow, Ui_MainWindow):
 	def __init__(self):
@@ -49,30 +39,32 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
 		self.Sonar = True
 		self.Lidar = False
 		self.Fusion = False
+		self.rep = 0
 		self.ang = 0
 		self.distS = 0
 		self.distL = 0
 		self.distF = 0
-		self.rdataS = np.zeros(64)
-		self.rdataL = np.zeros(64)
-		self.rdataF = np.zeros(64)
-		self.pdata = np.zeros(64)
+		self.rdataS = np.zeros(31)
+		self.rdataL = np.zeros(31)
+		self.rdataF = np.zeros(31)
+		self.pdata = np.zeros(31)
+		self.ax = self.canvas.figure.add_subplot(111, projection='polar')
 
 		#GRIDs
 		self.angles=[]
-		for k in range(1,31):
-			self.angles.append(-106+k*7)
+		for k in range(1,34):
+			self.angles.append(-117+k*7)
 
 		self.radii=[]
 		for k in range(1,9):
 			self.radii.append(k*10)
-		
+
 		#Crear parametros de funcionamiento
-		self.refresh=0.1 #Periodo de actualizacion de imagen
-		self.buffact=0.1 #Periodo de actualizacion de buffer de datos
+		self.refresh=0.01 #Periodo de actualizacion de imagen
+		#self.buffact=0.09 #Periodo de actualizacion de buffer de datos
 		self.buffready = False #Variable para indicar el fin de la lectura inicial del buffer de datos
 		self.refreshcount = self.refresh*1000
-		self.buffactcount = self.buffact*1000
+		#self.buffactcount = self.buffact*1000
 
 		#Iniciar timer para la graficacion
 		timergraf = QtCore.QTimer(self)
@@ -80,28 +72,32 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
 		timergraf.start(self.refreshcount)
 
 		#Iniciar timer para actualizacion de datos
-		timerbuff = QtCore.QTimer(self)
-		timerbuff.timeout.connect(self.lectura)
-		timerbuff.start(self.buffactcount)
+		#timerbuff = QtCore.QTimer(self)
+		#timerbuff.timeout.connect(self.lectura)
+		#timerbuff.start(self.buffactcount)
 		
 	def grafica(self):
-		self.ax = self.canvas.figure.add_subplot(111, projection='polar')
-		self.ax.clear()
-		if self.buffready:
-			self.radar = self.ax.plot(self.rad,[80,0],'g',alpha=0.35,drawstyle='steps')
-			if self.Sonar:
-				self.datosS = self.ax.plot(self.pdata, self.rdataS,'ro')
-			if self.Lidar:
-				self.datosL = self.ax.plot(self.pdata, self.rdataL,'bo')
-			if self.Fusion:
-				self.datosF = self.ax.plot(self.pdata, self.rdataF,'yo')
-		self.ax.set_theta_offset(np.pi/2)
-		self.ax.set_thetalim(thetamin=-106,thetamax=106)
-		self.ax.set_thetagrids(self.angles)
-		self.ax.set_rgrids(self.radii)
+		print(s.in_waiting)
+		if s.in_waiting:
+			self.lectura()
+			self.ax.clear()
+			if self.buffready: # Empieza a plotear una vez se realice una primera lectura
+				self.ax.plot(self.rad,[80,0],'g',alpha=0.35,drawstyle='steps') # Barrido del sonar
+				if self.Sonar:
+					self.ax.plot(self.pdata, self.rdataS,'ro') # Plot Sonar
+				if self.Lidar:
+					self.ax.plot(self.pdata, self.rdataL,'bo') # Plot Lidar
+				if self.Fusion:
+					self.ax.plot(self.pdata, self.rdataF,'yo') # Plot Fusion
+			self.ax.set_theta_offset(np.pi/2)
+			self.ax.set_thetalim(thetamin=-117,thetamax=117)
+			self.ax.set_thetagrids(self.angles)
+			self.ax.set_rgrids(self.radii)
 
-		self.canvas.draw()
-		self.canvas.flush_events()
+			self.canvas.draw()
+			self.canvas.flush_events()
+			#if(s.in_waiting>=8):
+			#	s.reset_input_buffer()
 
 	def lectura(self):
 		byte1 = s.read(1) #Lee un byte
@@ -110,44 +106,59 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
 		datos = s.read(3) #Si comienza por 0, lee el resto de los datos
 
 		# DECODIFICACION
-		self.posicion = (ord(byte1) & 63)
-		ang = (self.posicion-32)*3.7*np.pi/180
+		self.posicion = (ord(byte1) & 63) # Valor n que corresponde al angulo
+		ang = -(self.posicion-16)*7.4*np.pi/180 # Angulo en radianes
 		self.filtro = (ord(byte1)&64)>>6
-		self.distS = (((ord(datos[0]) & 127)<<2)|((ord(datos[1]) & 96)>>5))*61/58
+		self.distS = (((ord(datos[0]) & 127)<<2)|((ord(datos[1]) & 96)>>5))*61.035/58 # Si el valor sale del rango, se ajusta al maximo del Lidar
 		if self.distS > 80:
 			self.distS = 80
-		self.distL = ((ord(datos[1]) & 31)<<7)|(ord(datos[2]) & 127)
+		self.distL = ((ord(datos[1]) & 31)<<7)|(ord(datos[2]) & 127) # Si el valor sale del rango, se ajusta a los limites
 		if self.distL > 2300:
 			self.distL = 2300
 		elif self.distL < 400:
 			self.distL = 400
-		self.distL = 308135*self.distL**-1.382
+		self.distL = 308135*self.distL**-1.382 # Conversion a cm
+		#print(self.distL,self.distS)
 		
+		# Fusion
 		if self.distS < self.distL:
+			if self.distF == self.distS:
+				self.rep = self.rep+1
+			else:
+				self.rep = 0
 			self.distF = self.distS
+			if self.rep > 2:
+				self.distF = self.distL
+				self.rep = 0
 		else:
 			self.distF = self.distL
 
+		# Filtro
 		if self.filtro:
 			self.Filtro.setText('ON')
 		else:
 			self.Filtro.setText('OFF')
 
+		# Guardar los datos en un log
 		if self.buffready:
 			f = open('log.txt','a')
-			f.writelines('Angulo: ' + str((self.posicion-32)*3.7) + ' - Dist. Sonar: ' + str(self.distS) + ' - Dist. Lidar: ' + str(self.distL) + ' - Dist. Fusion: ' + str(self.distF) + '\n')
+			f.writelines('Angulo: ' + str(-(self.posicion-16)*7.4) + ' - Dist. Sonar: ' + str(self.distS) + ' - Dist. Lidar: ' + str(self.distL) + ' - Dist. Fusion: ' + str(self.distF) + '\n')
 			f.close()
 		else:
 			f = open('log.txt','w')
-			f.writelines('Log: \nAngulo: ' + str((self.posicion-32)*3.7) + ' - Dist. Sonar: ' + str(self.distS) + ' - Dist. Lidar: ' + str(self.distL) + ' - Dist. Fusion: ' + str(self.distF) + '\n')
+			f.writelines('Log: \nAngulo: ' + str(-(self.posicion-16)*7.4) + ' - Dist. Sonar: ' + str(self.distS) + ' - Dist. Lidar: ' + str(self.distL) + ' - Dist. Fusion: ' + str(self.distF) + '\n')
 			f.close()
 
+		# Actualiza los parametros a plotear
 		self.rad = [ang, 0]
-		self.rdataS[self.posicion] = self.distS
-		self.rdataL[self.posicion] = self.distL
-		self.rdataF[self.posicion] = self.distF
-		self.pdata[self.posicion] = ang
-		self.lcdNumber.display((self.posicion-32)*3.7)
+		self.rdataS[self.posicion-1] = self.distS
+		self.rdataL[self.posicion-1] = self.distL
+		self.rdataF[self.posicion-1] = self.distF
+		self.pdata[self.posicion-1] = ang
+		self.lcdNumber.display(-(self.posicion-16)*7.4)
+		self.lcdNumber_2.display(self.distS)
+		self.lcdNumber_3.display(self.distL)
+		self.lcdNumber_4.display(self.distF)
 		self.buffready = True
 
 	# FUNCIONES INTERFAZ
